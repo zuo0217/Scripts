f6c31f28ba61799451da13ef2b37f52ccf1866bc
zhouzuozheng <zuo0217@163.com>
2019-10-15 17:09:00 +0800

    modify checkIP
    modify checkIP

--- checkIP.py
+++ checkIP.py
@@ -5,12 +5,21 @@ import requests
 import re
 import sys
 
-URL = "https://www.baidu.com/s?wd={}"
 header = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"}
 
 
-def getInfo(ip):
+def getInfo():
+    try:
+        response = requests.get(url=URL, headers=header)
+        if response.status_code == 200:
+            tmp = re.findall('(\d+\.\d+\.\d+\.\d+)</span>([\u4e00-\u9fa5]+\s?[\u4e00-\u9fa5]+)', str(response.text))
+            if len(tmp) > 0:
+                return tmp[0][0], tmp[0][1]
+    except Exception as e:
+        return False, False
+
+def getInfoFrom(ip):
     try:
         response = requests.get(url=URL.format(ip), headers=header)
         if response.status_code == 200:
@@ -19,11 +28,37 @@ def getInfo(ip):
                 return True, ip, tmp[0]
     except Exception as e:
         pass
-    return False, ip, "查询失败"
+    return False, ip, "查询失败"        
 
 
+def getIP():
+    try:
+        response = requests.get(url="http://ip.360.cn/IPShare/info")
+        if response.status_code == 200:
+            data = response.json()
+            if data.get("ip", False) is not False:
+                return data["ip"]
+            else:
+                return "查询失败! {}".format(data)
+        else:
+            return "查询失败! HTTP CODE:{}".format(response.status_code)
+    except Exception as e:
+        return  "查询失败! {}".format(e)   
+
+def getInfoFromIP(ip):
+    try:
+        response = requests.post(url="http://ip.360.cn/IPQuery/ipquery", data={"ip":ip})
+        if response.status_code == 200:
+            data = response.json()
+            if data.get("data", False) is not False:
+                return data["data"]
+            else:
+                return "查询失败! {}".format(data)
+        else:
+            return "查询失败! HTTP CODE:{}".format(response.status_code)    
+    except Exception as e:
+        return  "查询失败! {}".format(e)     
+                         
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        ipAddress = sys.argv[1]
-        if re.findall(r'^\d+\.\d+\.\d+.\d+$', ipAddress):
-            print(getInfo(ipAddress))
+    print(getIP())
+    getInfoFromIP("122.192.9.78")
